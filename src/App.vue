d90970011802942853d357e18ba3e424b6e82772
刘师林 <2806489401@qq.com>
2020-10-21 18:02:02 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld :msg="msg" />
+</template>
+
+<script lang="ts">
+import { defineComponent,reactive } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+interface title_Data {
+    msg:string
+}
+
+export default defineComponent({
+   name: 'App',
+   components: {
+    HelloWorld
+    },
+    setup(){
+        const data:title_Data = reactive({
+            msg:'Welcome ~'
+        })
+
+        return {
+            ...data
+        }
+    }
+})
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+</template>
+
+<script lang="ts">
+export default {
+  props:['msg'],
+  data(){
+      return {
+          count:1
+      }
+  }
+}
+</script>
